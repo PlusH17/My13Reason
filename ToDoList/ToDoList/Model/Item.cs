ac968d48659a1738728646d7f41013e13b909b79
PlusH17 <hanapluskalova1@gmail.com>
2026-01-19 09:39:35 +0100

    Z hodiny - dodělat

---
+++ ToDoList/ToDoList/Model/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDOList.Model
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}
---
+++ ToDoList/ToDoList/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDOList.Model;
+using ToDOList.MVVM;
+
+namespace ToDOList.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        // Tento řádek vytváří příkaz pro tlačítko „Přidat“
+        // RelayCommand říká: "když někdo klikne, zavolej tuto metodu"
+        // execute => AddItem() znamená: ignorujeme parametr (exekuce), jen zavoláme metodu AddItem()
+        public RelayCommand AddCommand => new RelayCommand(execute => AddItem());
+
+        // Tento řádek vytváří příkaz pro tlačítko „Smazat“
+        // Tlačítko se spustí jen tehdy, pokud je vybraná nějaká položka (SelectedItem != null)
+        // execute => DeleteItem() říká, co se má stát po kliknutí
+        // canExecute => SelectedItem != null říká, zda je tlačítko aktivní
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteItem(), canExecute => SelectedItem != null);
+
+        // '=>' je jen zkrácený zápis pro malou funkci / metodu, kterou předáváme jako parametr.
+
+
+        public ObservableCollection<Item> Items { get; set; }
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+
+
+        }
+
+        private Item selectedItem;
+
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void AddItem()
+        {
+            Items.Add(new Item()
+            {
+                Name = "Princip of task",
+                Date = DateOnly.FromDateTime(DateTime.Now),
+
+            });
+        }
+
+        private void DeleteItem()
+        {
+            Items.Remove(SelectedItem);
+        }
+
+
+    }
+    
+}
