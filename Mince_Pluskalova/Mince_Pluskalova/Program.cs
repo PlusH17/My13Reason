e174e2f70dc2c140c693a1e2991bec01d94dab1b
PlusH17 <hanapluskalova1@gmail.com>
2026-03-09 12:31:38 +0100

    Backtracking MINCE

---
+++ Mince_Pluskalova/Mince_Pluskalova/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mince_Pluskalova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nacteni();
+        }
+
+        static void Nacteni()
+        {
+            #region Načítání dat
+            Console.WriteLine("Jaké mince máme k dispozici?");
+            List<int> typy_minci = new List<int>();
+            string[] typy = Console.ReadLine().Split();
+            for (int i = 0; i < typy.Length; i++)
+            {
+                typy_minci.Add(int.Parse(typy[i]));
+            }
+
+            Console.WriteLine("Jakou chcete hodnotu");
+            int suma = int.Parse(Console.ReadLine());
+            #endregion
+
+            #region Backtrecing
+            List<List<int>> vysledky = new List<List<int>>();
+
+            Backtrecing(suma, typy_minci, new List<int>(), 0, vysledky);
+            #endregion
+
+            #region Výsledky
+            if (vysledky.Count == 0)
+                Console.WriteLine("Neexistuje žádná kombinace mincí, která by dosáhla požadované hodnoty.");
+
+
+            foreach (var item in vysledky)
+            {
+                
+                Console.WriteLine(string.Join(" ", item));
+            }
+
+            Console.ReadLine();
+            #endregion 
+        }
+
+        static void Backtrecing(int suma, List<int> typy, List<int> pouzivam, int index_pridane, List<List<int>> vysledky)
+        {
+            if (suma == 0) 
+                return;
+            int soucet = pouzivam.Sum();
+
+            if (soucet == suma) //Našli jsme řešení (jupí), přidáme ho do výsledků
+            {
+                vysledky.Add(new List<int>(pouzivam));
+                return;
+            }
+
+            for (int i = index_pridane; i < typy.Count; i++) //Procházíme všechny typy mincí, které můžeme přidat (od indexu poslední přidané mince, ať se nevracíme zpátky)
+            {
+                if (soucet + typy[i] <= suma) //rovnou kontrola, jestli přidáním této mince nepřekročíme požadovanou hodnotu
+                {
+                    pouzivam.Add(typy[i]);
+                    Backtrecing(suma, typy, pouzivam, i, vysledky);
+                    pouzivam.RemoveAt(pouzivam.Count - 1); 
+                }
+            }
+        }
+    }
+}
---
+++ Mince_Pluskalova/Mince_Pluskalova/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Mince_Pluskalova")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mince_Pluskalova")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("07f2b7bc-750e-45cd-a173-43292dba3a9a")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
